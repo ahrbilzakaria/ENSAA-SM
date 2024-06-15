c647f227a0946b9f673b51297832ddefbe6e7665
ahrbilzakaria <zakariaahr@gmail.com>
2024-06-15 23:08:25 +0100

    google

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,11 @@
 import { AtSign } from "lucide-react";
 import Link from "next/link";
 import Button from "./ui/button";
+import { getAuthSession } from "@/lib/auth";
 
-export default function Navbar() {
+export default async function Navbar() {
+  const session = await getAuthSession();
+
   return (
     <nav className="fixed w-[80%] inset-0 h-fit mt-8 rounded-md mx-auto px-8 py-4 flex justify-between items-center gap-2 bg-gray-900 z-[50] ">
       <Link href="/" className="text-white flex gap-1">
@@ -10,18 +13,22 @@ export default function Navbar() {
         <p className="hidden md:block text-xl font-semibold">ENSAA-SM</p>
       </Link>
       {/*search bar*/}
-      <div className="flex gap-2">
-        <Link href="/signup" className="">
-          <Button className="text-white">
-            <p>Signup</p>
-          </Button>
-        </Link>
-        <Link href="/login" className="">
-          <Button className="bg-white">
-            <p>Login</p>
-          </Button>
-        </Link>
-      </div>
+      {session ? (
+        <p>Your loged in</p>
+      ) : (
+        <div className="flex gap-2">
+          <Link href="/signup" className="">
+            <Button className="text-white">
+              <p>Sign In</p>
+            </Button>
+          </Link>
+          <Link href="/login" className="">
+            <Button className="bg-white">
+              <p>Login</p>
+            </Button>
+          </Link>
+        </div>
+      )}
     </nav>
   );
 }
